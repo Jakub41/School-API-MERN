2596a184c18917b6bc14f16cc0fdfe2c51da78f8
jakub41 <lemiszewski@gmx.com>
2020-01-12 21:21:40 +0100

    Added  custom validation to model

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("../utilities/dbConnect");
 require("mongoose-type-email");
+const { toDate } = require("validator");
 
 const schema = {
     name: {
@@ -17,7 +18,11 @@ const schema = {
     },
     dateOfBirth: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: string => toDate(string),
+            message: "DoB invalid"
+        }
     },
     projects: [{
         type: mongoose.Types.ObjectId,
